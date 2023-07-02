4361da53e2333ebae735c9cdc4fd762fcd3ecd20
Marian Antonescu <129096443+MaxTheTurtle0@users.noreply.github.com>
2023-07-02 12:13:36 +0000

    Added day1 and day2

---
+++ 2022/day 2/rust/target/debug/deps/rust-67880f34428ad0e5.d
@@ -0,0 +1,5 @@
+/workspaces/advent-of-code/2022/day 2/rust/target/debug/deps/rust-67880f34428ad0e5: src/main.rs
+
+/workspaces/advent-of-code/2022/day 2/rust/target/debug/deps/rust-67880f34428ad0e5.d: src/main.rs
+
+src/main.rs:
---
+++ 2022/day 2/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/workspaces/advent-of-code/2022/day\ 2/rust/target/debug/rust: /workspaces/advent-of-code/2022/day\ 2/rust/src/main.rs
